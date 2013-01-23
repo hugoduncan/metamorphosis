b757eb056e8008d43ed1a7ce86e9ff7642430ed7
Hugo Duncan <hugo@hugoduncan.org>
2013-01-23 12:08:20 -0500

    Updated version for next release cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metamorphosis "0.1.0"
+(defproject metamorphosis "0.1.1-SNAPSHOT"
   :description "Macro protocols for clojure"
   :url "https://github.com/hugoduncan/metamorphosis"
   :license {:name "Eclipse Public License"
